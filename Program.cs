a16eb9586d1c00c991f0efd9b22285b8155bc263
Nem1ch13 <94758964+Nem1ch13@users.noreply.github.com>
2026-05-07 10:19:09 +0300

    Add using directive for Microsoft.AspNetCore.Http

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 class Program
 {
